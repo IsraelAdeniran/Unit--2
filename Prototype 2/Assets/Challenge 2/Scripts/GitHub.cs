b8be1207ead13b998d0ddb72fe84e3f2315f0f0b
IsraelAdeniran <B00157067@mytudublin.ie>
2023-10-16 09:53:59 +0100

    Challenge 2

---
+++ Prototype 2/Assets/Challenge 2/Scripts/GitHub.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitHub : MonoBehaviour
+{
+    public void openLink()
+    {
+        Application.OpenURL("https://github.com/IsraelAdeniran/Challenge--2.git");
+    }
+}
